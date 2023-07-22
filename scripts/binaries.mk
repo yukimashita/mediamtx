1368d442f5cb07d1efec13ba7e04f9d1dc1436e9
aler9 <46489434+aler9@users.noreply.github.com>
2023-07-22 13:29:45 +0200

    temp

--- scripts/binaries.mk
+++ scripts/binaries.mk
@@ -1,17 +1,30 @@
+RPICAMERA_BUILD_DEPENDENCIES = \
+	g++ \
+	pkg-config \
+	make \
+	libfreetype-dev \
+	xxd \
+	meson \
+	libyaml-dev \
+	python3-yaml \
+	python3-ply \
+	python3-jinja2 \
+	libssl-dev
+
 define DOCKERFILE_BINARIES
-FROM $(RPI32_IMAGE) AS rpicamera32
-RUN ["cross-build-start"]
-RUN apt update && apt install -y --no-install-recommends g++ pkg-config make libcamera-dev libfreetype-dev xxd patchelf
-WORKDIR /s/internal/rpicamera
-COPY internal/rpicamera .
-RUN cd exe && make -j$$(nproc)
+# FROM $(RPI32_IMAGE) AS rpicamera32
+# RUN ["cross-build-start"]
+# RUN apt update && apt install -y --no-install-recommends $(RPICAMERA_BUILD_DEPENDENCIES)
+# WORKDIR /s/internal/rpicamera/exe
+# COPY internal/rpicamera/exe .
+# RUN make -j$$(nproc)
 
 FROM $(RPI64_IMAGE) AS rpicamera64
 RUN ["cross-build-start"]
-RUN apt update && apt install -y --no-install-recommends g++ pkg-config make libcamera-dev libfreetype-dev xxd patchelf
-WORKDIR /s/internal/rpicamera
-COPY internal/rpicamera .
-RUN cd exe && make -j$$(nproc)
+RUN apt update && apt install -y --no-install-recommends $(RPICAMERA_BUILD_DEPENDENCIES)
+WORKDIR /s/internal/rpicamera/exe
+COPY internal/rpicamera/exe .
+RUN make -j$$(nproc)
 
 FROM $(BASE_IMAGE) AS build-base
 RUN apk add --no-cache zip make git tar
@@ -25,33 +38,33 @@ RUN rm -rf tmp binaries
 RUN mkdir tmp binaries
 RUN cp mediamtx.yml LICENSE tmp/
 
-FROM build-base AS build-windows-amd64
-RUN GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx.exe
-RUN cd tmp && zip -q ../binaries/mediamtx_$${VERSION}_windows_amd64.zip mediamtx.exe mediamtx.yml LICENSE
+# FROM build-base AS build-windows-amd64
+# RUN GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx.exe
+# RUN cd tmp && zip -q ../binaries/mediamtx_$${VERSION}_windows_amd64.zip mediamtx.exe mediamtx.yml LICENSE
 
-FROM build-base AS build-linux-amd64
-RUN GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
-RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_amd64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
+# FROM build-base AS build-linux-amd64
+# RUN GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
+# RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_amd64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
 
-FROM build-base AS build-darwin-amd64
-RUN GOOS=darwin GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
-RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_darwin_amd64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
+# FROM build-base AS build-darwin-amd64
+# RUN GOOS=darwin GOARCH=amd64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
+# RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_darwin_amd64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
 
-FROM build-base AS build-darwin-arm64
-RUN GOOS=darwin GOARCH=arm64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
-RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_darwin_arm64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
+# FROM build-base AS build-darwin-arm64
+# RUN GOOS=darwin GOARCH=arm64 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx
+# RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_darwin_arm64.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
 
-FROM build-base AS build-linux-armv6
-COPY --from=rpicamera32 /s/internal/rpicamera/exe/exe internal/rpicamera/exe/
-RUN GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx -tags rpicamera
-RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_armv6.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
-RUN rm internal/rpicamera/exe/exe
+# FROM build-base AS build-linux-armv6
+# COPY --from=rpicamera32 /s/internal/rpicamera/exe/exe internal/rpicamera/exe/
+# RUN GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx -tags rpicamera
+# RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_armv6.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
+# RUN rm internal/rpicamera/exe/exe
 
-FROM build-base AS build-linux-armv7
-COPY --from=rpicamera32 /s/internal/rpicamera/exe/exe internal/rpicamera/exe/
-RUN GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx -tags rpicamera
-RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_armv7.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
-RUN rm internal/rpicamera/exe/exe
+# FROM build-base AS build-linux-armv7
+# COPY --from=rpicamera32 /s/internal/rpicamera/exe/exe internal/rpicamera/exe/
+# RUN GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-X github.com/bluenviron/mediamtx/internal/core.version=$$VERSION" -o tmp/mediamtx -tags rpicamera
+# RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_armv7.tar.gz --owner=0 --group=0 mediamtx mediamtx.yml LICENSE
+# RUN rm internal/rpicamera/exe/exe
 
 FROM build-base AS build-linux-arm64
 COPY --from=rpicamera64 /s/internal/rpicamera/exe/exe internal/rpicamera/exe/
@@ -60,12 +73,12 @@ RUN tar -C tmp -czf binaries/mediamtx_$${VERSION}_linux_arm64v8.tar.gz --owner=0
 RUN rm internal/rpicamera/exe/exe
 
 FROM $(BASE_IMAGE)
-COPY --from=build-windows-amd64 /s/binaries /s/binaries
-COPY --from=build-linux-amd64 /s/binaries /s/binaries
-COPY --from=build-darwin-amd64 /s/binaries /s/binaries
-COPY --from=build-darwin-arm64 /s/binaries /s/binaries
-COPY --from=build-linux-armv6 /s/binaries /s/binaries
-COPY --from=build-linux-armv7 /s/binaries /s/binaries
+# COPY --from=build-windows-amd64 /s/binaries /s/binaries
+# COPY --from=build-linux-amd64 /s/binaries /s/binaries
+# COPY --from=build-darwin-amd64 /s/binaries /s/binaries
+# COPY --from=build-darwin-arm64 /s/binaries /s/binaries
+# COPY --from=build-linux-armv6 /s/binaries /s/binaries
+# COPY --from=build-linux-armv7 /s/binaries /s/binaries
 COPY --from=build-linux-arm64 /s/binaries /s/binaries
 endef
 export DOCKERFILE_BINARIES
